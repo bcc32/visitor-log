c43963a93c951b9b50b31d996cf351ca37ba8c9a
Aaron L. Zeng <a2z@mit.edu>
2017-01-29 22:42:44 -0500

    Refer to plain `program` within index.js

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const moment = require('moment');
-global.program = require('commander');
+const program = require('commander');
+
+global.program = program;
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-global.program
+program
   .version('0.1.0')
   .option('-p, --port <n>', 'specify port number (default: 80/8080)')
   .option('-d, --dbpath <path>', 'specify data directory (default: ./data)')
   .parse(process.argv);
 
-global.program.port = global.program.port || (isProduction ? 80 : 8080);
-global.program.dbpath = global.program.dbpath || './data';
+program.port = program.port || (isProduction ? 80 : 8080);
+program.dbpath = program.dbpath || './data';
 
 const api = require('./api');
 const msg = require('./msg');
@@ -42,6 +44,6 @@ app.use('/api', api);
 
 // Need to wait for DB to load the last key
 msg.lastKeyLoaded.then(() => {
-  console.log('Listening on port %d', global.program.port);
-  app.listen(global.program.port);
+  console.log('Listening on port %d', program.port);
+  app.listen(program.port);
 });
