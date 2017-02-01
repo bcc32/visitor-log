b232ada9160228a9c544e2380f779d6c54369d3a
Aaron L. Zeng <a2z@mit.edu>
2017-02-01 09:03:50 -0500

    Don't forget to mkdir -p

--- log.js
+++ log.js
@@ -1,13 +1,17 @@
+const mkdirp = require('mkdirp');
 const path = require('path');
 const winston = require('winston');
 require('winston-daily-rotate-file');
+
+const logDir = global.program.logDir;
+mkdirp.sync(logDir);
 
 const logger = new winston.Logger({
   transports: [
     new winston.transports.Console({ colorize: true }),
     new winston.transports.DailyRotateFile({
       level: global.isProduction ? 'info' : 'debug',
-      filename: path.join(global.program.logDir, 'log'),
+      filename: path.join(logDir, 'log'),
       datePattern: 'yyyy-MM-dd.',
       prepend: true,
     }),
