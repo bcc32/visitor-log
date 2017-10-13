d16bcf3ed7ee52f3f3c7d8583a7f0d780a27b98e
Aaron L. Zeng <a2z@mit.edu>
2017-10-13 16:25:26 -0400

    Gulpify client rollup

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,36 @@
+const glob      = require('glob');
 const gulp      = require('gulp');
+const minifyCSS = require('gulp-csso');
 const gzip      = require('gulp-gzip');
 const less      = require('gulp-less');
-const minifyCSS = require('gulp-csso');
 const replace   = require('gulp-replace');
+const merge     = require('merge-stream');
+const path      = require('path');
+const rollup    = require('rollup-stream');
+const babel     = require('rollup-plugin-babel');
+const commonjs  = require('rollup-plugin-commonjs');
+const resolve   = require('rollup-plugin-node-resolve');
+const uglify    = require('rollup-plugin-uglify');
+const source    = require('vinyl-source-stream');
+
+gulp.task('client', () => {
+  const plugins = [
+    resolve(),
+    commonjs(),
+  ];
+  if (process.env.NODE_ENV === 'production') {
+    plugins.push(babel({
+      exclude: 'node_modules/**',
+    }));
+    plugins.push(uglify());
+  }
+
+  return merge(glob.sync('client/*.js').map(input => {
+    return rollup({ input, plugins, format: 'iife' })
+      .pipe(source(path.resolve(input), path.resolve('client')));
+  }))
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('config', () => {
   return gulp.src('server/nginx.conf')
@@ -25,9 +53,10 @@ gulp.task('public', () => {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', [ 'config', 'css', 'public' ]);
+gulp.task('default', [ 'client', 'config', 'css', 'public' ]);
 
 gulp.task('watch', [ 'default' ], () => {
+  gulp.watch('client/*.js',       [ 'client' ]);
   gulp.watch('client/*.less',     [ 'css'    ]);
   gulp.watch('server/nginx.conf', [ 'config' ]);
   gulp.watch('public/*',          [ 'public' ]);
