95ac59686433b0aa75ce75937ba0d216647e9cf7
Aaron L. Zeng <a2z@mit.edu>
2017-10-14 22:23:41 -0400

    Better build error messages

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ const uglify        = require('gulp-uglify');
 const lazypipe      = require('lazypipe');
 const merge         = require('merge-stream');
 const path          = require('path');
+const pump          = require('pump');
 const rollup        = require('rollup-stream');
 const commonjs      = require('rollup-plugin-commonjs');
 const resolve       = require('rollup-plugin-node-resolve');
@@ -22,7 +23,12 @@ const source        = require('vinyl-source-stream');
 const isProd = process.env.NODE_ENV === 'production';
 
 gulp.task('bucklescript', (cb) => {
-  child_process.exec('./node_modules/.bin/bsb -make-world', cb);
+  child_process.exec('./node_modules/.bin/bsb -make-world', (err, stdout) => {
+    if (err != null) {
+      return cb(new Error(stdout.toString()));
+    }
+    cb();
+  });
 });
 
 const minifyJS = lazypipe()
@@ -30,8 +36,8 @@ const minifyJS = lazypipe()
       .pipe(babel)
       .pipe(uglify);
 
-gulp.task('client', [ 'bucklescript' ], () => {
-  return merge(glob.sync('client/*.js').map(input => {
+gulp.task('client', [ 'bucklescript' ], (cb) => {
+  const clientFiles = merge(glob.sync('client/*.js').map(input => {
     return rollup({
       input,
       format: 'iife',
@@ -41,58 +47,71 @@ gulp.task('client', [ 'bucklescript' ], () => {
       ],
     })
       .pipe(source(path.resolve(input), path.resolve('client')));
-  }))
-    .pipe(gulpif(isProd, minifyJS()))
-    .pipe(gulp.dest('dist'))
-    .pipe(gzip())
-    .pipe(gulp.dest('dist'));
+  }));
+  pump([
+    clientFiles,
+    gulpif(isProd, minifyJS()),
+    gulp.dest('dist'),
+    gzip(),
+    gulp.dest('dist'),
+  ], cb);
 });
 
-gulp.task('config', () => {
-  return gulp.src('server/nginx.conf')
-    .pipe(replace('PROJECT_ROOT', __dirname + '/'))
-    .pipe(gulp.dest(''));
+gulp.task('config', (cb) => {
+  pump([
+    gulp.src('server/nginx.conf'),
+    replace('PROJECT_ROOT', __dirname + '/'),
+    gulp.dest(''),
+  ], cb);
 });
 
-gulp.task('css', () => {
-  return gulp.src('client/less/*.less')
-    .pipe(less())
-    .pipe(gulpif(isProd, minifyCSS()))
-    .pipe(gulp.dest('dist'))
-    .pipe(gzip())
-    .pipe(gulp.dest('dist'));
+gulp.task('css', (cb) => {
+  pump([
+    gulp.src('client/less/*.less'),
+    less(),
+    gulpif(isProd, minifyCSS()),
+    gulp.dest('dist'),
+    gzip(),
+    gulp.dest('dist'),
+  ], cb);
 });
 
-gulp.task('public', () => {
-  return gulp.src('public/*')
-    .pipe(gzip())
-    .pipe(gulp.dest('dist'));
+gulp.task('public', (cb) => {
+  pump([
+    gulp.src('public/*'),
+    gzip(),
+    gulp.dest('dist'),
+  ], cb);
 });
 
-gulp.task('server', () => {
-  return gulp.src('server/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('bin'));
+gulp.task('server', (cb) => {
+  pump([
+    gulp.src('server/*.js'),
+    babel(),
+    gulp.dest('bin'),
+  ]);
 });
 
 gulp.task('default', [ 'client', 'config', 'css', 'public', 'server' ]);
 
-gulp.task('dist', [ 'default' ], () => {
-  // FIXME this is a hack
-  return gulp.src([
-    '**/*',
-    '!node_modules/**/*',
-    '!data.db',
-    '!lib/**/*',
-    '!logs/**/*',
-    '!dist.tar.gz',
-  ])
-    .pipe(rename((path) => {
+gulp.task('dist', [ 'default' ], (cb) => {
+  pump([
+    // FIXME this is a hack
+    gulp.src([
+      '**/*',
+      '!node_modules/**/*',
+      '!data.db',
+      '!lib/**/*',
+      '!logs/**/*',
+      '!dist.tar.gz',
+    ]),
+    rename((path) => {
       path.dirname = 'bcc32.com/' + path.dirname;
-    }))
-    .pipe(tar('dist.tar'))
-    .pipe(gzip())
-    .pipe(gulp.dest('.'));
+    }),
+    tar('dist.tar'),
+    gzip(),
+    gulp.dest('.'),
+  ], cb);
 });
 
 gulp.task('watch', [ 'default' ], () => {
